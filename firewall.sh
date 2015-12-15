3b755860724c71f69bb72d1ff68d1cd9a1ef4d53
Honeyshell <honeyshell@honeyshell.com>
2015-12-15 18:00:43 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -30,50 +30,50 @@
 
 
 # On efface les règles précédentes pour partir sur de bonnes bases
-sudo iptables -t filter -F 
-sudo iptables -t filter -X
+/sbin/iptables -t filter -F 
+/sbin/iptables -t filter -X
 
 # On bloque par défaut tout le trafic 
-sudo iptables -t filter -P INPUT DROP 
-sudo iptables -t filter -P FORWARD DROP 
-sudo iptables -t filter -P OUTPUT DROP
+/sbin/iptables -t filter -P INPUT DROP 
+/sbin/iptables -t filter -P FORWARD DROP 
+/sbin/iptables -t filter -P OUTPUT DROP
 
 # On ne ferme pas les connexions déjà établies
-sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
-sudo iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
+/sbin/iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # On autorise le loopback
-sudo iptables -t filter -A INPUT -i lo -j ACCEPT 
-sudo iptables -t filter -A OUTPUT -o lo -j ACCEPT
+/sbin/iptables -t filter -A INPUT -i lo -j ACCEPT 
+/sbin/iptables -t filter -A OUTPUT -o lo -j ACCEPT
 
 
 # Ouvrir les ports utilisés : 
 
 # ICMP (Ping)
-sudo iptables -t filter -A INPUT -p icmp -j ACCEPT 
-sudo iptables -t filter -A OUTPUT -p icmp -j ACCEPT 
+/sbin/iptables -t filter -A INPUT -p icmp -j ACCEPT 
+/sbin/iptables -t filter -A OUTPUT -p icmp -j ACCEPT 
  
 # SSH / HTTPS
-sudo iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT 
-sudo iptables -t filter -A OUTPUT -p tcp --dport 443 -j ACCEPT 
+/sbin/iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT 
+/sbin/iptables -t filter -A OUTPUT -p tcp --dport 443 -j ACCEPT 
 
 # Transmission
-sudo iptables -t filter -A OUTPUT -p tcp --dport 6001 -j ACCEPT 
-sudo iptables -t filter -A INPUT -p tcp --dport 6001 -j ACCEPT
+/sbin/iptables -t filter -A OUTPUT -p tcp --dport 6001 -j ACCEPT 
+/sbin/iptables -t filter -A INPUT -p tcp --dport 6001 -j ACCEPT
  
 # DNS
-sudo iptables -t filter -A OUTPUT -p tcp --dport 53 -j ACCEPT 
-sudo iptables -t filter -A OUTPUT -p udp --dport 53 -j ACCEPT 
-sudo iptables -t filter -A INPUT -p tcp --dport 53 -j ACCEPT 
-sudo iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT 
+/sbin/iptables -t filter -A OUTPUT -p tcp --dport 53 -j ACCEPT 
+/sbin/iptables -t filter -A OUTPUT -p udp --dport 53 -j ACCEPT 
+/sbin/iptables -t filter -A INPUT -p tcp --dport 53 -j ACCEPT 
+/sbin/iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT 
  
 # HTTP
-sudo iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT 
-sudo iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT 
+/sbin/iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT 
+/sbin/iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT 
 
 # FTP 
-# sudo iptables -t filter -A OUTPUT -p tcp --dport 20:21 -j ACCEPT 
-# sudo iptables -t filter -A INPUT -p tcp --dport 20:21 -j ACCEPT 
+# /sbin/iptables -t filter -A OUTPUT -p tcp --dport 20:21 -j ACCEPT 
+# /sbin/iptables -t filter -A INPUT -p tcp --dport 20:21 -j ACCEPT 
 
 # Mail SMTP 
 # iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT 
@@ -88,7 +88,7 @@ sudo iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
 # iptables -t filter -A OUTPUT -p tcp --dport 143 -j ACCEPT 
 
 # NTP (horloge du serveur) 
-sudo iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
+/sbin/iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
 
 # service		port d’écoute 	protocole
 # ssh			22				tcp
@@ -100,9 +100,9 @@ sudo iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
 # DNS			53				tcp et udp
 
 # Flood ou déni de service
-sudo iptables -A FORWARD -p tcp --syn -m limit --limit 1/second -j ACCEPT
-sudo iptables -A FORWARD -p udp -m limit --limit 1/second -j ACCEPT
-sudo iptables -A FORWARD -p icmp --icmp-type echo-request -m limit --limit 1/second -j ACCEPT
+/sbin/iptables -A FORWARD -p tcp --syn -m limit --limit 1/second -j ACCEPT
+/sbin/iptables -A FORWARD -p udp -m limit --limit 1/second -j ACCEPT
+/sbin/iptables -A FORWARD -p icmp --icmp-type echo-request -m limit --limit 1/second -j ACCEPT
 
 
 
